dd9e256f4d362968fc54b3663429882178e8a591
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-03 13:07:56 +0700

    init project

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+   <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+        <v-text-field
+            label="Email"
+            placeholder="Email"
+            solo
+          ></v-text-field>
+        </v-col>
+</template>
+
+<script>
+export default {
+
+}
+</script>
