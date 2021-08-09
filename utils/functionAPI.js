e08ee7b238d8867e03ca8010a0669a91d56cbce9
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-09 10:15:39 +0700

    connect API(edit,create,show)

--- utils/functionAPI.js
+++ utils/functionAPI.js
@@ -2,6 +2,7 @@ import { request } from "./API"
 const HOSTNAME = 'http://localhost:4000'
 
 export function index() {
+
     const url =`${HOSTNAME}/function`
     return request('get', url, {})
 }
@@ -9,9 +10,14 @@ export function show(id) {
     const url =`${HOSTNAME}/function/${id}`
     return request('get', url, {})
 }
-export function create(name, body) {
-    console.log('NAME', name)
-    console.log('BODY', body)
+export function create(group, choice) {
+    console.log('NAME', group)
+    console.log('BODY', choice)
     const url =`${HOSTNAME}/function/add`
-    return request('post', url, { name, body })
+    return request('post', url, { group, choice })
+}
+export function edit(id, group, choice) {
+    console.log(group, choice)
+    const url =`${HOSTNAME}/function/${id}/edit`
+    return request('put', url, { group, choice })
 }
