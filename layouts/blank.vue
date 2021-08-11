a0c77427263326ae281c8a3f65b36615164b7d46
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-11 18:50:04 +0700

    blank layout for login page

---
+++ layouts/blank.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app
+        fixed
+    >
+        <v-main>
+            <v-container>
+                <nuxt />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
