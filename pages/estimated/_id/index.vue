9c636cb1be325ef7baaec142984fda139fcc6403
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-11 10:37:40 +0700

    estimate page UI

---
+++ pages/estimated/_id/index.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <div class="mt-5 mx-5">
+            <h1>ชื่อระบบ:A</h1>
+            <h3>แพลตฟอร์ม:Website</h3>
+        </div>
+        <v-row
+            justify="start"
+            class="pb-5 px-5"
+        >
+            <v-card
+                width="400"
+                class="py-3 my-3"
+                flat
+            >
+                <v-card-content>
+                    <v-card-title class="text-h6 pa-2">
+                        จำนวนนักพัฒนา : 2
+                    </v-card-title>
+                    <v-card-title class="text-h6 pt-0 pb-0 pl-2">
+                        เวลาการพัฒนา : 2 weeks
+                    </v-card-title>
+                    <v-card-title class="text-h6 pa-2">
+                        ขนาดระบบ : กลาง (8-10 ฟังก์ชัน)
+                    </v-card-title>
+                </v-card-content>
+            </v-card>
+        </v-row>
+        <v-divider />
+        <div class="pt-5">
+            <h1>ฟังก์ชันที่เลือก</h1>
+        </div>
+        <div class="d-flex flex-wrap">
+            <v-card
+                class="ma-5 pa-5"
+                width="500px"
+                tile
+                v-for="i in 10"
+                :key="i"
+            >
+                <v-card-title class="text-h5 justify-center">
+                    ชื่อกลุ่มฟังก์ชัน
+                </v-card-title>
+                <div
+                    v-for="n in 3"
+                    :key="n"
+                >
+                    <v-list-item two-line>
+                        <v-list-item-avatar>
+                            <v-avatar
+                                class="ma-3 ml-6"
+                                color="red"
+                                size="120px"
+                            />
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>name</v-list-item-title>
+                            <v-list-item-subtitle>description</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </div>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h1 {
+    opacity: 70%;
+}
+h3 {
+
+    opacity: 70%;
+}
+</style>
---
+++ pages/estimated/index.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <div class="ma-3">
+            <h1>ผลการประมาณ</h1>
+        </div>
+        <v-data-table
+            :headers="data"
+            :items="desserts"
+            item-key="name"
+            class="elevation-0"
+            :search="search"
+            :custom-filter="filterOnlyCapsText"
+            :click="logs"
+        >
+            <template #top>
+                <v-text-field
+                    flat
+                    v-model="search"
+                    label="Search"
+                    class="mx-4"
+                    solo-inverted
+                />
+            </template>
+
+            <tr
+                @click="logs"
+            >
+                <td @click="logs" />
+                <td colspan="4" />
+            </tr>
+        </v-data-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            search: '',
+            calories: '',
+            desserts: [
+                {
+                    name: 'Frozen Yogurt',
+                },
+                {
+                    name: 'Ice cream sandwich',
+                },
+                {
+                    name: 'Eclair',
+                },
+                {
+                    name: 'Cupcake',
+                },
+                {
+                    name: 'Gingerbread',
+                },
+                {
+                    name: 'Jelly bean',
+                },
+                {
+                    name: 'Lollipop',
+                },
+                {
+                    name: 'Honeycomb',
+                },
+                {
+                    name: 'Donut',
+                },
+                {
+                    name: 'KitKat',
+                },
+            ],
+        }
+    },
+    computed: {
+        data () {
+            return [
+                {
+                    text: 'Name',
+                    align: 'start',
+                    sortable: true,
+                    value: 'name',
+                },
+
+            ]
+        },
+    },
+    methods: {
+        filterOnlyCapsText (value, search, item) {
+            return value != null &&
+          search != null &&
+          typeof value === 'string' &&
+          value.toString().toLocaleLowerCase().indexOf(search) !== -1
+        },
+        logs() {
+            console.log('abc')
+        }
+
+    },
+}
+</script>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
