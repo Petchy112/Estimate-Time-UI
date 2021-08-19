40abe2eae3e4a403f59d064e94b7b876e8ce55ec
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-19 18:40:07 +0700

    remove catch error

--- utils/API.js
+++ utils/API.js
@@ -1,15 +1,11 @@
 import axios from 'axios'
-export async function request(method, url, data, headers) {
-    try {
-        const response = await axios({
-            method, url, data, headers
-        })
-        return response
-    }
 
-    catch (error) {
-        console.log('error in API', error)
-        return error
-    }
+export async function request(method, url, data, headers) {
+
+    const response = await axios({
+        method, url, data, headers
+    })
+    return response
+
 
 }
