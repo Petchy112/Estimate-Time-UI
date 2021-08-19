007dfa2ce418d20bb096d9409bc06f85cd1b9b17
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-19 18:40:23 +0700

    show estimate

--- utils/estimateAPI.js
+++ utils/estimateAPI.js
@@ -5,3 +5,7 @@ export function index() {
     const url =`${HOSTNAME}/estimate`
     return request('get', url, {}, { Authorization: localStorage.getItem('token') })
 }
+export function show(id) {
+    const url = `${HOSTNAME}/estimate/${id}`
+    return request('get', url, {}, { Authorization: localStorage.getItem('token') })
+}
