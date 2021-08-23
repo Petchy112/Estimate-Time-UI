45bf319aee8b3457c127900c0687f4981ffa2249
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 18:31:33 +0700

    config blank

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,12 +1,8 @@
 <template>
-    <v-app
-        fixed
-    >
-        <v-main>
-            <v-container>
-                <Nuxt />
-            </v-container>
-        </v-main>
+    <v-app>
+        <v-container>
+            <Nuxt />
+        </v-container>
     </v-app>
 </template>
 
@@ -16,6 +12,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.v-application{
+    font-family: 'Mitr', sans-serif !important;
+    background: gray;
+}
 
+.v-application--wrap {
+    justify-content: center !important;
+}
+
+.w-100{
+ width: 100%;
+}
 </style>
