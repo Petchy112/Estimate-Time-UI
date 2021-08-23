da6a127e263aac56163f727e7336acfc70393c32
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 23:37:44 +0700

    result vote details liff

---
+++ pages/liff/result/_id/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="voteData">
+        <v-tabs
+            v-model="tab"
+            slider-color="yellow"
+            fixed-tabs
+        >
+            <v-tab
+                v-for="item in items"
+                :key="item.tab"
+            >
+                {{ item.tab }}
+            </v-tab>
+        </v-tabs>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'liff'
+
+
+}
+</script>
+
+<style>
+
+</style>
