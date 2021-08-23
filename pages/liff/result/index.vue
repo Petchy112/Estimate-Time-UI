28f258180d9157bfeed848b1bac92ba966c1e04a
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 23:37:28 +0700

    result vote liff

---
+++ pages/liff/result/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-col cols="12">
+            <h1>Vote Results</h1>
+        </v-col>
+        <voteComponant />
+    </div>
+</template>
+
+<script>
+import voteComponant from "~/components/vote/index.vue"
+import * as voteAPI from "@/utils/voteAPI"
+export default {
+    layout: 'liff',
+    components: {
+        voteComponant
+    },
+    data () {
+        return {
+            results: [],
+        }
+    },
+    async mounted() {
+        await voteAPI.index()
+            .then(response => {
+                console.log('RESPONSE', response)
+                this.results = response.data
+            })
+            .catch(async error => {
+                console.log('ERROR', error.response)
+                this.message = error.response.data.error.message
+            })
+
+    },
+
+    methods: {
+        async handleShowClicked(id) {
+            console.log(id)
+            this.$router.push({ name: 'vote-id', params: { id } })
+        }
+    }
+}
+</script>
+
+<style>
+.v-btn {
+ height: 50px;
+}
+</style>
