ede22874dd732f05130ce149e69e5aea7398c1c2
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 18:32:26 +0700

    account manage liff

---
+++ pages/account.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-col cols="12">
+            <v-card elevation="5">
+                <v-list>
+                    <v-col cols="12">
+                        <div class="d-flex flex-column pa-2">
+                            <v-btn class="role-btn" color="yellow" @click="changepassword">
+                                Change password
+                            </v-btn>
+                            <v-btn class="role-btn" color="red" @click="logout">
+                                Sign out
+                            </v-btn>
+                        </div>
+                    </v-col>
+                </v-list>
+            </v-card>
+        </v-col>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'blank',
+    methods: {
+        async logout() {
+            await userAPI.logout()
+                .then(response => {
+                    console.log(response)
+                    localStorage.clear()
+                    this.$router.replace({ name: 'login' })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        async changepassword() {
+            await this.$router.push({ name: 'changepassword' })
+        }
+
+    }
+
+}
+
+</script>
+
+<style>
+
+.v-list__title {
+    font-weight: bold;
+}
+.role-btn {
+    margin-top: 20px;
+    height: 50px !important;
+    font-size: 16px;
+}
+</style>
