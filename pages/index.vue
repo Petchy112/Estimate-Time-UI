77125cc27d6c1f0c2fb5d40f4b2f3d1d3e45ddf2
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 21:59:03 +0700

    change index to login

---
+++ pages/index.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <v-card>
+            <v-card-title>
+                <div class="my-header">
+                    ESTIMATE TIME APP
+                </div>
+            </v-card-title>
+            <v-card-subtitle>
+                Please login to website
+            </v-card-subtitle>
+            <v-card-action>
+                <v-row>
+                    <v-col cols="12">
+                        <v-form
+                            ref="form"
+                            v-model="valid"
+                            lazy-validation
+                        >
+                            <v-text-field
+                                v-model="body.email"
+                                :rules="emailRules"
+                                label="Email"
+                                solo-inverted
+                                required
+                            />
+                            <v-text-field
+                                v-model="body.password"
+                                :rules="passwordRules"
+                                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                                :type="show ? 'text' : 'password'"
+                                @click:append="show = !show"
+                                label="Password"
+                                solo-inverted
+                                required
+                            />
+
+
+                            <v-btn
+                                class="width-100"
+                                color="orange"
+                                rounded
+                                @click="login"
+                                dark
+                            >
+                                Sign in
+                            </v-btn>
+                        </v-form>
+                    </v-col>
+                </v-row>
+            </v-card-action>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import * as userAPI from "@/utils/userAPI"
+export default {
+    layout: 'blank',
+    data () {
+        return {
+            valid: true,
+            message: '',
+            body: {
+                email: '',
+                password: ''
+            },
+            show: false,
+            passwordRules: [
+                v => !!v || 'Password is Required.',
+                v => (v && v.length >= 8) || 'Min 8 characters',
+            ],
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+        }
+    },
+    methods: {
+        async login() {
+            this.$refs.form.validate()
+            await userAPI.login(this.body)
+                .then(async response => {
+                    console.log('RESPONSE', response)
+                    await localStorage.setItem('token', response.data.accessToken)
+                    this.$router.push({ name: 'function' })
+                })
+                .catch(async error => {
+                    console.log('ERROR', error.response)
+                    this.message = error.response.data.error.message
+                })
+        }
+    }
+}
+</script>
+
+<style>
+.v-card-subtitle {
+    padding-bottom: 10px 0;
+}
+.width-100 {
+    width: 100%;
+    margin-bottom:9px ;
+}
+.v-form {
+    padding: 0 20px;
+}
+.my-header{
+    font-weight: bold;
+    font-size: x-large;
+}
+</style>
