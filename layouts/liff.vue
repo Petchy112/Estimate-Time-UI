a5eb3af4aee0772228e5736947037eb69e59369b
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-23 23:34:44 +0700

    create for liff page

---
+++ layouts/liff.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-container>
+            <Nuxt />
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.v-application{
+    font-family: 'Mitr', sans-serif !important;
+}
+.w-100{
+ width: 100%;
+}
+
+</style>
