9da20595c41bc3ffc0113861ee22242a2fb35eea
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-24 16:51:59 +0700

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        titleTemplate: '%s - Estimate-Time-UI',
-        title: 'Estimate-Time-UI',
+        titleTemplate: '%s',
+        title: 'Estimate Time',
         htmlAttrs: {
             lang: 'html'
         },
