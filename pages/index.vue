4fdbeb2eaad6109d5b54d3d5bb13dd537ca2f835
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-27 16:37:50 +0700

    create login companant

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,22 @@
 <template>
     <div>
-        <v-col cols="12">
-            <v-card>
-                <v-card-title>
-                    <div class="my-header">
-                        ESTIMATE TIME APP
-                    </div>
-                </v-card-title>
-                <v-card-subtitle>
-                    Please login to website
-                </v-card-subtitle>
-
-                <v-row>
-                    <v-col cols="12">
-                        <v-form
-                            ref="form"
-                            v-model="valid"
-                            lazy-validation
-                        >
-                            <v-text-field
-                                v-model="body.email"
-                                :rules="emailRules"
-                                label="Email"
-                                solo-inverted
-                                required
-                            />
-                            <v-text-field
-                                v-model="body.password"
-                                :rules="passwordRules"
-                                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                                :type="show ? 'text' : 'password'"
-                                @click:append="show = !show"
-                                label="Password"
-                                solo-inverted
-                                required
-                            />
-
-
-                            <v-btn
-                                class="width-100"
-                                color="orange"
-                                rounded
-                                @click="login"
-                                dark
-                            >
-                                Sign in
-                            </v-btn>
-                        </v-form>
-                    </v-col>
-                </v-row>
-            </v-card>
-        </v-col>
+        <login @login-clicked="login" />
     </div>
 </template>
 
 <script>
 import * as userAPI from "@/utils/userAPI"
+import login from "~/components/auth/login.vue"
 export default {
-    layout: 'blank',
-    data () {
-        return {
-            valid: true,
-            message: '',
-            body: {
-                email: '',
-                password: ''
-            },
-            show: false,
-            passwordRules: [
-                v => !!v || 'Password is Required.',
-                v => (v && v.length >= 8) || 'Min 8 characters',
-            ],
-            emailRules: [
-                v => !!v || 'E-mail is required',
-                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-            ],
-        }
+    components: {
+        login
     },
+    layout: 'blank',
+
+
     methods: {
-        async login() {
-            this.$refs.form.validate()
-            await userAPI.login(this.body)
+        async login(body) {
+            await userAPI.login(body)
                 .then(async response => {
                     console.log('RESPONSE', response)
                     await localStorage.setItem('token', response.data.accessToken)
