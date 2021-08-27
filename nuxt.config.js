eb02c41e464fb506e3f7f30518c477ac191d47fa
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-27 21:01:38 +0700

    include line script

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ export default {
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
             { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Mitr:wght@200;400;600&display=swap" }
+        ],
+        script: [
+            { src: "https://static.line-scdn.net/liff/edge/2.1/sdk.js" }
         ]
     },
 
