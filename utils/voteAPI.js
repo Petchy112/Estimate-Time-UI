5350373b4531d44682d15334037e4b23a9285757
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-27 14:16:58 +0700

    add choose platform

--- utils/voteAPI.js
+++ utils/voteAPI.js
@@ -5,8 +5,8 @@ export function index() {
     const url =`${HOSTNAME}/vote`
     return request('get', url, {}, { Authorization: localStorage.getItem('token') })
 }
-export function show(date) {
-    const url =`${HOSTNAME}/vote/${date}`
+export function show(date, platform) {
+    const url =`${HOSTNAME}/vote/${date}?platform=${platform}`
     return request('get', url, {}, { Authorization: localStorage.getItem('token') })
 }
 export function sentVote(fid, body) {
