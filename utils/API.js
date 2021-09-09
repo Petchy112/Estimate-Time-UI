883a3e198886232c043c7b7ef4a4f00c902d5b57
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-09-09 14:53:34 +0700

    update

--- utils/API.js
+++ utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const HOSTNAME = 'https://estimate-time.herokuapp.com'
+export const HOSTNAME = 'https://estimate-time.herokuapp.com'//'http://localhost:4000'
 export async function request(method, url, data, headers) {
 
     const response = await axios({
