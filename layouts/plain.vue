971c870137da04fcba4d17c96a6f7d0f61123f4e
Kittikan Ditkhanan <mju6104101307@mju.ac.th>
2022-01-27 20:02:04 +0700

    add layout

---
+++ layouts/plain.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrap-plain">
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+
+export default {
+
+
+}
+</script>
+
+<style lang="scss">
+@import '~/styles/variables.scss';
+.wrap-plain {
+    width: 100%;
+    background-color: #fff
+}
+
+
+</style>
