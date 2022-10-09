41f7f970f33e28222069f3a67cb1ae3a95d864a5
Kittikan Ditkhanan <mju6104101307@mju.ac.th>
2022-10-09 22:46:42 +0700

    fix bug storeage

--- store/estimate.js
+++ store/estimate.js
@@ -3,7 +3,7 @@ export const state = () => ({
     platform: '',
     estimateTime: 0,
     projectName: '',
-    quantity: 1,
+    qty: 1,
 })
 
 export const mutations = {
@@ -11,14 +11,16 @@ export const mutations = {
         state.selectedChoice = data.selectedChoice
         state.platform = data.platform
         state.estimateTime = data.estimateTime
-        state.projectName = data.projectName
-        state.quantity = data.quantity
+        state.qty = data.qty
     },
     resetEstimateData(state) {
         state.selectedChoice = []
         state.platform = ''
         state.estimateTime = 0
         state.projectName = ''
-        state.quantity = 1
+        state.qty = 1
+    },
+    setName(state, projectName) {
+        state.projectName = projectName
     },
 }
